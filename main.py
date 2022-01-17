6f82bc522814210b61971671b4a041f866129de3
clément degreve-naillon <clement@MacBook-Pro-de-clement.local>
2022-01-17 14:30:27 +0100

    Update file

--- main.py
+++ main.py
@@ -14,8 +14,16 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 ts = str(calendar.timegm(time.gmtime())) + "000"
 
-if temperature is not None:
-    print(ts + ',' '{0:0.1f}'.format(temperature))
+if str(sys.argv[1]) == 'temperature':
+    if temperature is not None:
+        print(ts + ',' '{0:0.1f}'.format(temperature))
+    else:
+        print('Err.')
+        sys.exit(1)
 
-if humidity is not None:
-    print(ts + ',' + '{0:0.1f}'.format(humidity))
+elif str(sys.argv[1]) == 'humidity':
+    if humidity is not None:
+        print(ts + ',' + '{0:0.1f}'.format(humidity))
+    else:
+        print('Err.')
+        sys.exit(1)
