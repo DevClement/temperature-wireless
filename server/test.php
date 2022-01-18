7c88f3c38cb528a527de6a5252dafffc7b307c67
criss1987 <48321751+criss1987@users.noreply.github.com>
2022-01-18 13:35:28 +0100

    Update test.php

--- server/test.php
+++ server/test.php
@@ -1,48 +1,70 @@
 <!DOCTYPE html>
-<center>
+
     <head>
         <style>
-            h1 {color:red;}
-            table {color:blue;}
-
+            h1 {
+                text-align:center;
+                font-family: Tahoma, sans-serif;
+            }
+            table {
+                align-self: center;
+            }
+            .wrapper{
+                display: flex;
+                flex-direction: row;
+                font-family: Tahoma, sans-serif;
+            }
+            .gauche{
+                width: 50%;
+                height: 100vh;
+                text-align: center;
+            }
+            .droite{
+                width: 50%;
+                height: 100vh;
+                text-align: center;
+            }
         </style>
     </head>
-
-        <h1>Température et humidité</h1>
-
+        <h1>Données de température et humidité de la pièce </h1>
     <?php
-    echo "<html><body><table>\n\n";
-    // Open a files
+        // Open a files
+        $fileTemp = fopen("temperature.csv", "r");
+        $fileHum = fopen("humidity.csv", "r");
 
-    $fileTemp = fopen("/home/pi/temperature-wireless/temperature.csv", "r");
-    $fileHum = fopen("/home/pi/temperature-wireless/humidity.csv", "r");
+        echo '<div class="wrapper">';
+            echo '<div class="gauche">';
+                echo "<table>";
+                    echo"Température";
+                    // Fetching data from csv file row by row
+                    while (($data = fgetcsv($fileTemp)) !== false) {
+                        // HTML tag for placing in row format
+                        echo "<tr>";
+                            foreach ($data as $i) {
+                                echo "<td>" . htmlspecialchars($i) . "</td>";
+                            }
+                        echo "</tr>\n";
+                        echo"<tr><td><br/></td></tr>";
+                    }
+                echo "</table>";
+            echo '</div>';
 
-    // Fetching data from csv file row by row
-    while (($data = fgetcsv($fileTemp)) !== false) {
-
-        // HTML tag for placing in row format
-        echo "<tr>";
-
-        foreach ($data as $i) {
-            echo "<td>" . htmlspecialchars($i)
-                . "</td>";
-        }
-        echo "</tr> \n";
-
-    }
-    while (($data = fgetcsv($fileHum)) !== false) {
-
-        // HTML tag for placing in row format
-        echo "<tr>";
-
-        foreach ($data as $j) {
-            echo "<td>" . htmlspecialchars($j)
-                . "</td>";
-        }
-        echo "</tr> \n";
-    }
-    // Closing the files
-    fclose($fileTemp);
-    fclose($fileHum);
-    echo "\n</table></center></body></html>";
+            echo '<div class="droite">';
+                echo "<table>";
+                    echo"Humidité";
+                    // Fetching data from csv file row by row
+                    while (($data = fgetcsv($fileHum)) !== false) {
+                        // HTML tag for placing in row format
+                        echo "<tr>";
+                        foreach ($data as $j) {
+                            echo "<td>" . htmlspecialchars($j) . "</td>";
+                        }
+                        echo "</tr> \n";
+                        echo"<tr><td><br/></td></tr>";
+                     }
+                echo "</table>";
+            echo '</div>';
+        echo '</div>';
+        // Closing the files
+        fclose($fileHum);
     ?>
