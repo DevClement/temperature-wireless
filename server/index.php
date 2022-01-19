1d4c0fe860e227aa4ccbc7aeec5fc0a9c94a890b
GregoryValsaint <gvalsaint@live.fr>
2022-01-19 11:01:00 +0100

    final front

--- server/index.php
+++ server/index.php
@@ -1,9 +1,22 @@
 <?php 
 
-$csvFile = file('/home/pi/temperature-wireless/temperature.csv');
-$data = [];
-foreach ($csvFile as $line) {
-    $data[] = str_getcsv($line);
+$tempCsvFile = file("/home/pi/temperature-wireless/temperature.csv");
+$humidCsvFile = file("/home/pi/temperature-wireless/humidity.csv");
+$datasTemp = [];
+$datasHumid = [];
+
+foreach ($tempCsvFile as $line) {
+    $datasTemp[] = str_getcsv($line);
 }
 
-var_dump($data);
+foreach ($datasTemp as $data){
+    $temperature = $data[1];
+}
+
+foreach ($humidCsvFile as $line) {
+    $datasHumid[] = str_getcsv($line);
+}
+
+foreach ($datasHumid as $data){
+    $humidity = $data[1];
+}
